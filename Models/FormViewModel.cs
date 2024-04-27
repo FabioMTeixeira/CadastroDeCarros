4a670cc4ab9ba9fdac742667775ae6730bcc237e
fabio <fabio.matosteixeira@gmail.com>
2024-04-27 14:17:11 -0300

    feat: Add new mvc
    This commit initiates the creation of an mvc to add the fuel used by the user to the database

---
+++ Models/FormViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CadastroDeCarros.Models
+{
+    public class FormViewModel
+    {
+        public int Id { get; set; }
+        public required string Descricao { get; set; }
+        public required string Status { get; set; }
+    }
+}
